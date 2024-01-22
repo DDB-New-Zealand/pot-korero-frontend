8fc947232723cb5008d7c7f437e74063a698d85f
Victor Potasso <victorpotasso@gmail.com>
2024-01-22 13:13:11 +1300

    updated yml

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,6 +1,6 @@
 export default function Test() {
     return (
-        <div className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+        <div className="grid gap-2 text-white">
             <p>var TEST: {process.env.TEST}</p>
             <p>var NEXT_PUBLIC_TEST: {process.env.NEXT_PUBLIC_TEST}</p>
         </div>
